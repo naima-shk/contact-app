aeac757f38e7a1365617090832f6bc46892a69db
naima-shk <naimashaikh550@gmail.com>
2021-08-23 19:22:40 +0500

    'implement-contact-list-application'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import Card from './component/Card.js'
 import './App.css';
+import React ,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Card/>
-    </div>
-  );
+
+
+const Contacts = [
+  {
+    id: 1,
+    name: 'Daryl Otoole',
+    email: 'daryl.otoole@example.com'
+
+  //  avatarURL: 'http://localhost:5001/tyler.jpg'
+  },
+  {
+    id: 2,
+    name: 'Gavin Harris',
+    email: 'gavin.harris@example.com'
+    //avatarURL: 'http://localhost:5001/karen.jpg'
+  },
+  {
+    id: 3,
+    name: 'Sibylla Krenz',
+    email: 'sibylla.krenz@example.com'
+
+    //avatarURL: 'http://localhost:5001/richard.jpg'
+  },
+]
+
+class App extends Component{
+   render(){
+     return(
+       <div>
+       <Card contacts={Contacts}/>
+       </div>
+     )
+   }
 }
-
 export default App;
--- src/component/Card.js
+++ src/component/Card.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React,{Component} from 'react'
 
-const Card = (props) =>(
-  <div>
-  <h2>naima shaikh</h2>
+class Card extends Component{
+  render(){
+  //  console.log('props',this.props)
+    return(
+      <ol className='card-list'>
+      {this.props.contacts.map((contact)=>(
+        <li key={contact.id} className='contact-list-item'>
+        <div className='contact-avatar'>
+        </div>
+        <div className='contact-details'>
+        <p>  {contact.name}</p>
+        <p>  {contact.email}</p>
+        </div>
+        </li>
+      )
 
-    <div>
-    email address
-    </div>
-</div>
-
-
-)
+      )}
+      </ol>
+    )
+  }
+}
 
 export default Card
